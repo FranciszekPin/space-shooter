0b59c800ff31c7220dc77e9155c22a029fecb773
pumassv1 <pumass775@gmail.com>
2020-12-29 00:06:04 +0100

    Add GameManager class

---
+++ GameManager.cpp
@@ -0,0 +1,7 @@
+//
+// Created by pumassv1 on 28.12.2020.
+//
+#include <stdio.h>
+#include "GameManager.h"
+
+
---
+++ GameManager.h
@@ -0,0 +1,14 @@
+//
+// Created by pumassv1 on 28.12.2020.
+//
+
+#ifndef SPACE_SHOOTER_GAMEMANAGER_H
+#define SPACE_SHOOTER_GAMEMANAGER_H
+
+
+class GameManager {
+
+};
+
+
+#endif //SPACE_SHOOTER_GAMEMANAGER_H
