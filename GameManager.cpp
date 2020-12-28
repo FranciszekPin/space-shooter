414e2af49cb3b9bf94c093134049a15c8bcdba9d
pumassv1 <pumass775@gmail.com>
2020-12-29 00:53:44 +0100

    Add initializing game

--- GameManager.cpp
+++ GameManager.cpp
@@ -3,5 +3,36 @@
 //
 #include <stdio.h>
 #include "GameManager.h"
+#include "constants.h"
+
+
+GameManager::GameManager() {
+    window = NULL;
+    renderer = NULL;
+}
+
+bool GameManager::init() {
+    bool success = true;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else {
+        window = SDL_CreateWindow("Space Shooter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        } else {
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+            if (renderer == NULL) {
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                success = false;
+            } else {
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            }
+        }
+    }
+    return success;
+}
 
 
--- GameManager.h
+++ GameManager.h
@@ -5,9 +5,18 @@
 #ifndef SPACE_SHOOTER_GAMEMANAGER_H
 #define SPACE_SHOOTER_GAMEMANAGER_H
 
+#include "sdlsystem.h"
 
 class GameManager {
+private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
 
+public:
+    GameManager();
+    bool init();
+    void startGame();
+    ~GameManager();
 };
 
 
