e14fc9c1f69e7a49bc4a4a6ac73e5e2e97e163cf
macpietrwdev <maciekpmail@gmail.com>
2021-01-18 20:24:14 +0100

    Monster visibility improvement

--- Enemy.cpp
+++ Enemy.cpp
@@ -2,16 +2,26 @@
 #include "Enemy.h"
 #include "constants.h"
 #include <string>
-#include"sdlsystem.h"
+#include "sdlsystem.h"
 
 
 
 
-Enemy::Enemy(SDL_Renderer *renderer, const char imgSrc)
+Enemy::Enemy(SDL_Renderer* renderer, const char* imgSrc)
 {
 	SDL_Surface* tmpSurface = IMG_Load(imgSrc);
-	enemyIMG = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	enemyImg = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	if (!enemyImg)
+		printf("Failed to create texture\n");
 	SDL_FreeSurface(tmpSurface);
+	if (!tmpSurface)
+		printf("Failed to create surface\n");
+	this->renderer = renderer;
+	position.x = 0;
+	position.y = 0;
+	position.h = 64;
+	position.w = 64;
+	SDL_RenderCopy(renderer, enemyImg, NULL, &position);
 }
 
 Enemy::~Enemy()
@@ -24,12 +34,12 @@ void Enemy::move()
 {
 	position.y += velocity;
 	if (position.x <= 0)
-		movevector = false;
+		moveVector = false;
 	if (position.x >= SCREEN_WIDTH - 50)
-		movevector = true;
-	if (movevector == true)
-		position.x -= 4 * velocity;
+		moveVector = true;
+	if (moveVector == true)
+		position.x -= velocity;
 	else
-		position.x += 4 * velocity;
-	SDL_RenderCopy(renderer, enemyIMG, NULL, &position);
+		position.x += velocity;
+	SDL_RenderCopy(renderer, enemyImg, NULL, &position);
 }
